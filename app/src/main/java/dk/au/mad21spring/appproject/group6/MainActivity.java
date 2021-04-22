f29b17e4b462b01fe952913b3ec5b7c4af21d9ce
Mikkel-KR <Mikkel.kora@gmail.com>
2021-04-22 14:49:32 +0200

    Tilføjet request-fragment og lavet support for at skifte imellem fragments.

--- app/src/main/java/dk/au/mad21spring/appproject/group6/MainActivity.java
+++ app/src/main/java/dk/au/mad21spring/appproject/group6/MainActivity.java
@@ -1,12 +1,13 @@
 package dk.au.mad21spring.appproject.group6;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
-import com.google.android.material.tabs.TabItem;
 import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,14 +15,39 @@ public class MainActivity extends AppCompatActivity {
 
     TextView helloTest;
     TabLayout mainTabs;
+    ConstraintLayout mainFragmentContainer;
+
+    //Fragment Tags
+    private static final String BEVERAGE_LIST_FRAG = "beverage_list_fragment";
+    private static final String BEVERAGE_DETAILS_FRAG = "beverage_details_fragment";
+    private static final String REQUESTS_FRAG = "requests_fragment";
+    private static final String PROFILE_FRAG = "profile_fragment";
+
+    //Fragments
+    private ListFragment beverageListFragment;
+    private RequestFragment requestFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        setupUI();
+    }
+
+    private void setupUI() {
         helloTest = findViewById(R.id.main_hello_test);
         mainTabs = findViewById(R.id.main_tabs);
+        mainFragmentContainer = findViewById(R.id.main_fragment_containter);
+
+        beverageListFragment = ListFragment.newInstance("", "");
+        requestFragment = RequestFragment.newInstance("", "");
+
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.main_fragment_containter, beverageListFragment, BEVERAGE_LIST_FRAG)
+                .add(R.id.main_fragment_containter, requestFragment, REQUESTS_FRAG)
+                .replace(R.id.main_fragment_containter, beverageListFragment, BEVERAGE_LIST_FRAG)
+                .commit();
 
         mainTabs.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
@@ -29,9 +55,15 @@ public class MainActivity extends AppCompatActivity {
                 switch(tab.getPosition()) {
                     case 0:
                         helloTest.setText("Hello List");
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.main_fragment_containter, beverageListFragment, BEVERAGE_LIST_FRAG)
+                                .commit();
                         break;
                     case 1:
                         helloTest.setText("Hello Request");
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.main_fragment_containter, requestFragment, REQUESTS_FRAG)
+                                .commit();
                         break;
                     case 2:
                         helloTest.setText("Hello Profile");
@@ -52,6 +84,5 @@ public class MainActivity extends AppCompatActivity {
                 // Go to original fragment for the selected tab
             }
         });
-
     }
 }
